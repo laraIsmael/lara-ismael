10661ba8a1122037e31c29608d6225a55e09b2df
Lara Ismael <laraismael@icloud.com>
2024-05-29 14:53:48 -0700

    resume page done

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,7 +1,19 @@
+import { ArrowDownTrayIcon } from '@heroicons/react/24/solid'
+
+
 export default function Resume() {
   return (
-    <div className="flex flex-col font-display py-10 px-32 bg-stone-100 w-fill h-fit">
-      <p>Resume</p>
+    <div className="flex flex-col font-display py-10 px-32 bg-stone-100 w-fill h-fill">
+      <div className="flex justify-between">
+        <p className="pb-5 ml-8">Resume</p>
+        <div  className='w-5 h-5 mr-8 top-4 cursor-pointer'>
+          <a href="./LaraIsmael-resume.pdf" download>
+            <ArrowDownTrayIcon />
+          </a>
+        </div>
+      </div>
+
+      <img src="./resume-screenshot.png" alt="resume" />
     </div>
   )
 }
